c0d9157fb4122c0e40f403dc464f7d2089cd3cae
NickChlam <Nick.Chlam@rht.comm>
2020-08-25 21:05:35 -0600

    push

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -10,4 +10,4 @@ export AWS_VIRTUAL_HOST=ecs.nickchlam.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-# optional: SERVICESTACK_LICENSE
+# option: SERVICESTACK_LICENSE
